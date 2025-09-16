0a064ce72fe914d48cda625673ab63b086bf592f
Nash Thames <taylor.thames@atlasschool.com>
2025-09-15 21:27:38 -0500

    sign bytes without hash

--- crypto/ec_sign.c
+++ crypto/ec_sign.c
@@ -12,24 +12,21 @@
 uint8_t *ec_sign(
 	EC_KEY const *key, uint8_t const *msg, size_t msglen, sig_t *sig)
 {
-	uint8_t digest[SHA256_DIGEST_LENGTH];			/* message digest */
 	unsigned int der_sig_len = 0;					/* DER-encoded sig len */
 	int max_len;									/* max sig length */
 
 	if (!key || !msg || !sig)						/* input checks */
 		return (NULL);
 
-	if (!SHA256(msg, msglen, digest))				/* hash message */
-		return (NULL);
-
 	max_len = ECDSA_size(key);						/* make sure sig fits */
 	if (max_len <= 0 || max_len > SIG_MAX_LEN)		/* inside buffer */
 		return (NULL);
 
-	if (ECDSA_sign(0, digest, SHA256_DIGEST_LENGTH,	/* sign the digest */
-		sig->sig, &der_sig_len, (EC_KEY *)key) != 1)
-		return (NULL);
+	if (ECDSA_sign(
+		0, msg, msglen, sig->sig, &der_sig_len, (EC_KEY *)key) != 1)
+		return (NULL);								/* sign the message */
 
 	sig->len = (uint8_t)der_sig_len;				/* store DER sig length */
+
 	return (sig->sig);								/* return sig pointer */
 }
