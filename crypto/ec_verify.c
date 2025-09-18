174a11d80077411a96d33adc589f494a5c5f41ff
Nash Thames <taylor.thames@atlasschool.com>
2025-09-17 22:47:48 -0500

    switched to verifying raw msg

--- crypto/ec_verify.c
+++ crypto/ec_verify.c
@@ -1,7 +1,7 @@
 #include "hblk_crypto.h"
 
 /**
- * ec_verify -	verifies a message signature with an EC public key
+ * ec_verify -	verifies a raw message signature with an EC public key
  * @key:		EC key pair with public key
  * @msg:		bytes that were signed
  * @msglen:		number of bytes in msg
@@ -12,16 +12,11 @@
 int ec_verify(
 	EC_KEY const *key, uint8_t const *msg, size_t msglen, sig_t const *sig)
 {
-	uint8_t digest[SHA256_DIGEST_LENGTH];		/* SHA-256 digest of message */
-
 	if (!key || !msg || !sig || sig->len == 0)	/* input checks */
 		return (0);
 
-	if (SHA256(msg, msglen, digest) == NULL)	/* hash message first */
-		return (0);
-
-	if (ECDSA_verify(0, digest, SHA256_DIGEST_LENGTH, sig->sig, sig->len,
-		(EC_KEY *)key) != 1)					/* verify signature */
+	if (ECDSA_verify(0, msg, msglen, sig->sig, sig->len,
+		(EC_KEY *)key) != 1)					/* verify raw signature */
 		return (0);
 
 	return (1);									/* signature is valid */
