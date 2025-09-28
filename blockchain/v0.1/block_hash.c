1f605bf6d559faecd32081307078c615ab1c72f6
Nash Thames <taylor.thames@atlasschool.com>
2025-09-28 16:24:19 -0500

    data length!

--- blockchain/v0.1/block_hash.c
+++ blockchain/v0.1/block_hash.c
@@ -10,29 +10,33 @@
 uint8_t *block_hash(block_t const *block,
 	uint8_t hash_buf[SHA256_DIGEST_LENGTH])
 {
-	SHA256_CTX context;						/* block hash context */
+	SHA256_CTX context;
 
-	if (!block || !hash_buf)				/* check inputs */
+	if (!block || !hash_buf)
 		return (NULL);
 
-	if (SHA256_Init(&context) != 1)			/* init context */
+	if (SHA256_Init(&context) != 1)
 		return (NULL);
 
 	if (SHA256_Update(&context, &block->info, sizeof(block->info)) != 1)
-		return (NULL);						/* hash block info */
+		return (NULL);
 
-	if (block->data.len > 0)				/* valid data to hash */
+	if (SHA256_Update(&context, &block->data.len,
+		sizeof(block->data.len)) != 1)
+		return (NULL);
+
+	if (block->data.len > 0)
 	{
 		size_t data_len = block->data.len;
 
-		if (data_len > BLOCKCHAIN_DATA_MAX)	/* cap data length */
+		if (data_len > BLOCKCHAIN_DATA_MAX)
 			data_len = BLOCKCHAIN_DATA_MAX;
 		if (SHA256_Update(&context, block->data.buffer, data_len) != 1)
-			return (NULL);					/* hash block data */
+			return (NULL);
 	}
 
-	if (SHA256_Final(hash_buf, &context) != 1)	/* finalize hash */
+	if (SHA256_Final(hash_buf, &context) != 1)
 		return (NULL);
 
-	return (hash_buf);						/* pointer to hash buffer */
+	return (hash_buf);
 }
