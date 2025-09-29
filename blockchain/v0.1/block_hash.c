34e60337cadcf1c90dff7312d1a3224ee353875f
Nash Thames <taylor.thames@atlasschool.com>
2025-09-28 20:03:34 -0500

    refactored block_hash to exclude data length

--- blockchain/v0.1/block_hash.c
+++ blockchain/v0.1/block_hash.c
@@ -1,15 +1,16 @@
 #include "blockchain.h"
 
 /**
- * block_hash -		computes the SHA256 hash of a block's info and data
+ * block_hash -		generates the SHA256 hash of a block
  * @block:			block pointer
  * @hash_buf:		output buffer
  * Return:			pointer to hash_buf or NULL on failure
  */
-uint8_t *block_hash(block_t const *block,
-	uint8_t hash_buf[SHA256_DIGEST_LENGTH])
+uint8_t *block_hash(
+	block_t const *block, uint8_t hash_buf[SHA256_DIGEST_LENGTH])
 {
 	SHA256_CTX context;							/* SHA256 context */
+	size_t data_len;							/* length of block data */
 
 	if (!block || !hash_buf)					/* validate inputs */
 		return (NULL);
@@ -21,8 +22,11 @@ uint8_t *block_hash(block_t const *block,
 		sizeof(block->info)) != 1)
 		return (NULL);
 
-	if (SHA256_Update(&context, &block->data,	/* hash block data */
-		sizeof(block->data)) != 1)
+	data_len = block->data.len;					/* get data length */
+	if (data_len > BLOCKCHAIN_DATA_MAX)			/* cap data length */
+		data_len = BLOCKCHAIN_DATA_MAX;
+	if (data_len &&								/* update context */
+	    SHA256_Update(&context, block->data.buffer, data_len) != 1)
 		return (NULL);
 
 	if (SHA256_Final(hash_buf, &context) != 1)	/* finalize hash */
