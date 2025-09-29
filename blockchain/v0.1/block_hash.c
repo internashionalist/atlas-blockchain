228f1bf7776cafb5fc60da81de75384df5485d7c
Nash Thames <taylor.thames@atlasschool.com>
2025-09-28 19:50:35 -0500

    maybe block_hash is the problem with blockchain_serialize?

--- blockchain/v0.1/block_hash.c
+++ blockchain/v0.1/block_hash.c
@@ -1,42 +1,32 @@
 #include "blockchain.h"
 
 /**
- * block_hash - Compute the SHA-256 hash of a block
- * @block: Pointer to the block to hash
- * @hash_buf: Buffer where the resulting hash is stored
- *
- * Return: Pointer to @hash_buf on success, NULL on failure
+ * block_hash -		computes the SHA256 hash of a block's info and data
+ * @block:			block pointer
+ * @hash_buf:		output buffer
+ * Return:			pointer to hash_buf or NULL on failure
  */
 uint8_t *block_hash(block_t const *block,
 	uint8_t hash_buf[SHA256_DIGEST_LENGTH])
 {
-	SHA256_CTX context;
+	SHA256_CTX context;							/* SHA256 context */
 
-	if (!block || !hash_buf)
+	if (!block || !hash_buf)					/* validate inputs */
 		return (NULL);
 
-	if (SHA256_Init(&context) != 1)
+	if (SHA256_Init(&context) != 1)				/* init context */
 		return (NULL);
 
-	if (SHA256_Update(&context, &block->info, sizeof(block->info)) != 1)
+	if (SHA256_Update(&context, &block->info,	/* hash block info */
+		sizeof(block->info)) != 1)
 		return (NULL);
 
-	if (SHA256_Update(&context, &block->data.len,
-		sizeof(block->data.len)) != 1)
+	if (SHA256_Update(&context, &block->data,	/* hash block data */
+		sizeof(block->data)) != 1)
 		return (NULL);
 
-	if (block->data.len > 0)
-	{
-		size_t data_len = block->data.len;
-
-		if (data_len > BLOCKCHAIN_DATA_MAX)
-			data_len = BLOCKCHAIN_DATA_MAX;
-		if (SHA256_Update(&context, block->data.buffer, data_len) != 1)
-			return (NULL);
-	}
-
-	if (SHA256_Final(hash_buf, &context) != 1)
+	if (SHA256_Final(hash_buf, &context) != 1)	/* finalize hash */
 		return (NULL);
 
-	return (hash_buf);
+	return (hash_buf);							/* return hash buffer */
 }
