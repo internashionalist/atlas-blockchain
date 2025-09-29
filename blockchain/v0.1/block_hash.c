e6897fe0e93ff4b075379b4a13f22205e1f9d93c
Nash Thames <taylor.thames@atlasschool.com>
2025-09-29 14:38:28 -0500

    I made it too hard. SHA256 FTW

--- blockchain/v0.1/block_hash.c
+++ blockchain/v0.1/block_hash.c
@@ -9,28 +9,11 @@
 uint8_t *block_hash(
 	block_t const *block, uint8_t hash_buf[SHA256_DIGEST_LENGTH])
 {
-	SHA256_CTX context;							/* SHA256 context */
-	size_t data_len;							/* length of block data */
-
-	if (!block || !hash_buf)					/* validate inputs */
+	if (!block || !hash_buf)						/* check for NULL */
 		return (NULL);
 
-	if (SHA256_Init(&context) != 1)				/* init context */
-		return (NULL);
+	SHA256((unsigned char *)&block->info,			/* hash info and data */
+	sizeof(block->info) + block->data.len, hash_buf);
 
-	if (SHA256_Update(&context, &block->info,	/* hash block info */
-		sizeof(block->info)) != 1)
-		return (NULL);
-
-	data_len = block->data.len;					/* get data length */
-	if (data_len > BLOCKCHAIN_DATA_MAX)			/* cap data length */
-		data_len = BLOCKCHAIN_DATA_MAX;
-	if (data_len &&								/* update context */
-	    SHA256_Update(&context, block->data.buffer, data_len) != 1)
-		return (NULL);
-
-	if (SHA256_Final(hash_buf, &context) != 1)	/* finalize hash */
-		return (NULL);
-
-	return (hash_buf);							/* return hash buffer */
+	return (hash_buf);								/* return hash buffer */
 }
