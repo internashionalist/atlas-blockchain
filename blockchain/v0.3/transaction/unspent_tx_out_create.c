faf8903e0d789c06aca3878f81b32c8327175b2f
Nash Thames <taylor.thames@atlasschool.com>
2025-10-29 15:14:03 -0500

    something about deprecated EC_KEY-free

--- blockchain/v0.3/transaction/unspent_tx_out_create.c
+++ blockchain/v0.3/transaction/unspent_tx_out_create.c
@@ -13,3 +13,20 @@ unspent_tx_out_t *unspent_tx_out_create(
 	uint8_t block_hash[SHA256_DIGEST_LENGTH],
 	uint8_t tx_id[SHA256_DIGEST_LENGTH],
 	tx_out_t const *out)
+{
+	unspent_tx_out_t *unspent;					/* unspent tx output */
+
+	if (!block_hash || !tx_id || !out)			/* validate inputs */
+		return (NULL);
+
+	unspent = malloc(sizeof(*unspent));			/* allocate memory */
+	if (!unspent)
+		return (NULL);
+
+												/* init memory & set fields */
+	memcpy(unspent->block_hash, block_hash, SHA256_DIGEST_LENGTH);
+	memcpy(unspent->tx_id, tx_id, SHA256_DIGEST_LENGTH);
+	memcpy(&unspent->out, out, sizeof(unspent->out));
+
+	return (unspent);							/* return unspent tx output */
+}
