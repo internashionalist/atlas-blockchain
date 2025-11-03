0bc10171563b50cb378f9a16f41881b695c0ea87
Nash Thames <taylor.thames@atlasschool.com>
2025-11-03 14:40:31 -0600

    handles new block

--- blockchain/v0.3/blockchain_create.c
+++ blockchain/v0.3/blockchain_create.c
@@ -18,25 +18,26 @@ static void blockchain_cleanup(blockchain_t *blockchain)
 }
 
 /**
- * blockchain_create -	creates and initializes a blockchain
- *						with the genesis block
+ * blockchain_create -			creates and initializes a blockchain
+ *								with the genesis block
  *
- * Return:				pointer to the new blockchain
- *						or NULL on failure
+ * Return:						pointer to the new blockchain
+ *								or NULL on failure
  */
 blockchain_t *blockchain_create(void)
 {
 	blockchain_t *blockchain = NULL;			/* new blockchain container */
 	block_t *genesis = NULL;					/* gen block representation */
 	block_data_t gen_data = {"Holberton School", 16};	/* gen block data */
-	block_info_t gen_info = {0, 0, 1537578000, 0, {0}};		/* gen block info */
+	block_info_t gen_info = {0, 0, 1537578000, 0, {0}};	/* gen block info */
 
 	blockchain = malloc(sizeof(*blockchain));	/* allocate memory - chain */
 	if (!blockchain)
 		return (NULL);
-	memset(blockchain, 0, sizeof(*blockchain));	/* ensure clean slate */
+	blockchain->chain = NULL;					/* initialize chain */
+	blockchain->unspent = NULL;					/* initialize unspent */
 	blockchain->unspent = llist_create(MT_SUPPORT_FALSE); /* create unspent */
-	blockchain->chain = llist_create(MT_SUPPORT_TRUE);	/* create list */
+	blockchain->chain = llist_create(MT_SUPPORT_FALSE);	/* create chain list */
 	if (!blockchain->unspent || !blockchain->chain)
 	{
 		blockchain_cleanup(blockchain);
@@ -50,11 +51,17 @@ blockchain_t *blockchain_create(void)
 	}
 	genesis->info = gen_info;						/* set genesis info */
 	genesis->data = gen_data;						/* set genesis data */
-	genesis->transactions = NULL;					/* no transactions */
+	genesis->transactions = llist_create(MT_SUPPORT_FALSE); /* init tx list */
+	if (!genesis->transactions)
+	{
+		free(genesis);
+		blockchain_cleanup(blockchain);
+		return (NULL);
+	}
 	memcpy(genesis->hash, HLBTN_HASH, sizeof(genesis->hash)); /* copy genesis */
 	if (llist_add_node(blockchain->chain, genesis, ADD_NODE_REAR) != 0)
 	{												/* add genesis to chain */
-		free(genesis);
+		block_destroy(genesis);
 		blockchain_cleanup(blockchain);
 		return (NULL);
 	}
