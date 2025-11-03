d0de53dd5e43fe45b0921960096da0154fba0a7e
Nash Thames <taylor.thames@atlasschool.com>
2025-11-03 14:32:50 -0600

    should pass valgrind now

--- blockchain/v0.3/blockchain_create.c
+++ blockchain/v0.3/blockchain_create.c
@@ -1,6 +1,23 @@
 #include "blockchain.h"
 
 /**
+ * blockchain_cleanup -			helper to free blockchain on failure
+ * @blockchain:					blockchain to free
+ *
+ * Return:						void
+ */
+static void blockchain_cleanup(blockchain_t *blockchain)
+{
+	if (!blockchain)
+		return;
+	if (blockchain->chain)
+		llist_destroy(blockchain->chain, 0, NULL);
+	if (blockchain->unspent)
+		llist_destroy(blockchain->unspent, 0, NULL);
+	free(blockchain);
+}
+
+/**
  * blockchain_create -	creates and initializes a blockchain
  *						with the genesis block
  *
@@ -14,36 +31,32 @@ blockchain_t *blockchain_create(void)
 	block_data_t gen_data = {"Holberton School", 16};	/* gen block data */
 	block_info_t gen_info = {0, 0, 1537578000, 0, {0}};		/* gen block info */
 
-
 	blockchain = malloc(sizeof(*blockchain));	/* allocate memory - chain */
 	if (!blockchain)
 		return (NULL);
-
+	memset(blockchain, 0, sizeof(*blockchain));	/* ensure clean slate */
+	blockchain->unspent = llist_create(MT_SUPPORT_FALSE); /* create unspent */
 	blockchain->chain = llist_create(MT_SUPPORT_TRUE);	/* create list */
-	if (!blockchain->chain)
+	if (!blockchain->unspent || !blockchain->chain)
 	{
-		free(blockchain);
+		blockchain_cleanup(blockchain);
 		return (NULL);
 	}
-
 	genesis = malloc(sizeof(*genesis));		/* allocate memory - gen block */
 	if (!genesis)
 	{
-		llist_destroy(blockchain->chain, 0, NULL);	/* must destroy list */
-		free(blockchain);							/* before freeing chain */
+		blockchain_cleanup(blockchain);
 		return (NULL);
 	}
-
 	genesis->info = gen_info;						/* set genesis info */
 	genesis->data = gen_data;						/* set genesis data */
+	genesis->transactions = NULL;					/* no transactions */
 	memcpy(genesis->hash, HLBTN_HASH, sizeof(genesis->hash)); /* copy genesis */
 	if (llist_add_node(blockchain->chain, genesis, ADD_NODE_REAR) != 0)
 	{												/* add genesis to chain */
 		free(genesis);
-		llist_destroy(blockchain->chain, 0, NULL);
-		free(blockchain);
+		blockchain_cleanup(blockchain);
 		return (NULL);
 	}
-
 	return (blockchain);						/* ptr to new blockchain */
 }
--- blockchain/v0.3/blockchain_destroy.c
+++ blockchain/v0.3/blockchain_destroy.c
@@ -8,7 +8,11 @@ void blockchain_destroy(blockchain_t *blockchain)
 {
 	if (!blockchain)
 		return;
-	llist_destroy(blockchain->chain, 1, NULL);	/* destroy list and blocks */
-	free(blockchain);							/* free blockchain struct */
-	blockchain = NULL;							/* nullify pointer */
+	if (blockchain->chain)
+		llist_destroy(blockchain->chain, 1,
+			(node_dtor_t)block_destroy);		/* destroy blocks */
+	if (blockchain->unspent)
+		llist_destroy(blockchain->unspent, 1, free); /* destroy unspent */
+	free(blockchain);							/* free blockchain */
+	blockchain = NULL;							/* nullify blockchain */
 }
