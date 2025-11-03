4fbfec4be1866897c7a4225321db6ca73cb1cca9
Nash Thames <taylor.thames@atlasschool.com>
2025-11-02 23:31:42 -0600

    betty

--- blockchain/v0.3/transaction/coinbase_create.c
+++ blockchain/v0.3/transaction/coinbase_create.c
@@ -12,48 +12,48 @@ transaction_t *coinbase_create(
 	EC_KEY const *receiver,
 	uint32_t block_index)
 {
-    transaction_t *transaction;					/* new transaction */
-    tx_in_t *input;								/* transaction input */
-    tx_out_t *output;							/* transaction output */
-    uint8_t receiver_pub[EC_PUB_LEN];			/* receiver's public key */
+	transaction_t *transaction;					/* new transaction */
+	tx_in_t *input;								/* transaction input */
+	tx_out_t *output;							/* transaction output */
+	uint8_t receiver_pub[EC_PUB_LEN];			/* receiver's public key */
 
-    if (!receiver)								/* no receiver */
-        return (NULL);
+	if (!receiver)								/* no receiver */
+		return (NULL);
 
-    transaction = calloc(1, sizeof(*transaction));	/* allocate transaction */
-    if (!transaction)
-        return (NULL);
+	transaction = calloc(1, sizeof(*transaction));	/* allocate transaction */
+	if (!transaction)
+		return (NULL);
 											/* create input & output lists */
-    transaction->inputs = llist_create(MT_SUPPORT_FALSE);
-    transaction->outputs = llist_create(MT_SUPPORT_FALSE);
-    if (!transaction->inputs || !transaction->outputs)
-        goto fail;
+	transaction->inputs = llist_create(MT_SUPPORT_FALSE);
+	transaction->outputs = llist_create(MT_SUPPORT_FALSE);
+	if (!transaction->inputs || !transaction->outputs)
+		goto fail;
 
-    input = calloc(1, sizeof(*input));			/* create input */
-    if (!input)
-        goto fail;
+	input = calloc(1, sizeof(*input));			/* create input */
+	if (!input)
+		goto fail;
 												/* set input values */
-    memcpy(input->tx_out_hash, &block_index, sizeof(block_index));
+	memcpy(input->tx_out_hash, &block_index, sizeof(block_index));
 												/* add input to transaction */
-    if (llist_add_node(transaction->inputs, input, ADD_NODE_REAR) == -1)
-        goto fail;
+	if (llist_add_node(transaction->inputs, input, ADD_NODE_REAR) == -1)
+		goto fail;
 		input = NULL;							/* reset */
 
-    if (!ec_to_pub(receiver, receiver_pub))		/* get receiver public key */
-        goto fail;
+	if (!ec_to_pub(receiver, receiver_pub))		/* get receiver public key */
+		goto fail;
 
-    output = tx_out_create(COINBASE_AMOUNT, receiver_pub);	/* create output */
-    if (!output)
-        goto fail;
+	output = tx_out_create(COINBASE_AMOUNT, receiver_pub);	/* create output */
+	if (!output)
+		goto fail;
 												/* add output to transaction */
-    if (llist_add_node(transaction->outputs, output, ADD_NODE_REAR) == -1)
-        goto fail;
+	if (llist_add_node(transaction->outputs, output, ADD_NODE_REAR) == -1)
+		goto fail;
 		output = NULL;							/* reset */
 												/* compute transaction hash */
-    if (!transaction_hash(transaction, transaction->id))
-        goto fail;
+	if (!transaction_hash(transaction, transaction->id))
+		goto fail;
 
-    return (transaction);						/* successful tx creation */
+	return (transaction);						/* successful tx creation */
 
 fail:
 	if (input)
